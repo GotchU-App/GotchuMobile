8f81c29e1be847dfbc370e0f363918120d6c70f4
Antoine Rakotozafy <antoine.rakotozafy@gmail.com>
2017-08-22 16:13:05 -0700

    Create different pages to work on them individually

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController } from 'ionic-angular';
 
+/**
+ * Generated class for the HomePage tabs.
+ *
+ * See https://angular.io/docs/ts/latest/guide/dependency-injection.html for
+ * more info on providers and Angular DI.
+ */
+
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  mainRoot = 'MainPage'
+  profileRoot = 'ProfilePage'
+  introRoot = 'IntroPage'
+  testRoot = 'TestPage'
 
-  }
+
+  constructor(public navCtrl: NavController) {}
 
 }
