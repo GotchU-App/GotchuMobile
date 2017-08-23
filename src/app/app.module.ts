3564c63fa31f15f445f509c7a84fd56acfd3527e
Antoine Rakotozafy <antoine.rakotozafy@gmail.com>
2017-08-22 17:40:19 -0700

    Beginning of the front for the main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage,
     LoginPage
   ],
   imports: [
@@ -21,7 +19,6 @@ import { LoginPage } from '../pages/login/login';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage,
     LoginPage
 
   ],
